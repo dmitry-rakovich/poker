b0b97d6502f1d9c36f64f6eb70f92ab24239f935
Dmitry Rakovich <dmitriy_ra@holdingbp.ru>
2025-02-01 14:34:44 +0300

    feat: add playerInfo

--- src/components/PlayerInfo/PlayerInfo.jsx
+++ src/components/PlayerInfo/PlayerInfo.jsx
@@ -1,6 +1,18 @@
+import { useState } from "react";
+import "./PlayerInfo.less";
 const PlayerInfo = () => {
+    const [isOpen, setIsOpen] = useState(false);
     return (
-        <div>PlayerInfo</div>
+        <div className="player-info">
+            <p className="player-info__name">LongUserName</p>
+            <div className="player-info__avatar">
+                <img onClick={() => { }} src="/avatar.png" />
+            </div>
+            <div className="player-info__balance">
+                <img onClick={() => { setIsOpen(!isOpen) }} src={isOpen ? "/visible.svg" : "/unvisible.svg"} />
+                <span>{isOpen ? "$100,500.00" : "****"}</span>
+            </div>
+        </div>
     )
 }
 
