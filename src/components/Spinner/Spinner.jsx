1268ade8f13e3029ca050d0302fcc455c71548e6
Dmitry Rakovich <dmitriy_ra@holdingbp.ru>
2025-02-01 14:16:02 +0300

    feat: add spinner

---
+++ src/components/Spinner/Spinner.jsx
@@ -0,0 +1,23 @@
+import "./Spinner.less";
+const Spinner = () => {
+    return (
+        <div className="spinner-container">
+            <div className="spinner">
+                <div className="dot"></div>
+                <div className="dot"></div>
+                <div className="dot"></div>
+                <div className="dot"></div>
+                <div className="dot"></div>
+                <div className="dot"></div>
+                <div className="dot"></div>
+                <div className="dot"></div>
+                <div className="dot"></div>
+                <div className="dot"></div>
+                <div className="dot"></div>
+                <div className="dot"></div>
+            </div>
+        </div>
+    )
+}
+
+export default Spinner
